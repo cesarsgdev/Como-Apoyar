e1bfa3a38c6e713e33ff92ea41a6b54f40af2c0d
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-06-04 00:35:09 -0500

    Created centers DB model

---
+++ models/centersModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const uuid = require("uuid");
+
+const centersSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      default: uuid.v4,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      maxlength: 50,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 50,
+    },
+
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+
+    instructions: {
+      type: String,
+      required: false,
+      trim: true,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const centersModel = mongoose.model("centers", centersSchema);
+
+module.exports = centersModel;
