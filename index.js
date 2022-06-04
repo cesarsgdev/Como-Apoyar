102260480b778b979ed9baf4664df54e35f408fe
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-06-03 23:47:17 -0500

    Added disasters route to index.js to expose it

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const centers = require("./routes/centers");
+const disasters = require("./routes/disasters");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/centers", centers);
+app.use("/api/disasters", disasters);
 
 app.get("/", (req, res) => {
   res.status(200).send("This is the placeholder API endpoint");
